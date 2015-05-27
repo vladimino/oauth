00cfb42a46aafcc9dbf50ec97eaf457c204c0fa3
Vladimino <vladimino@gmail.com>
2015-05-28 01:49:48 +0300

    Client Credentials

--- cron/collect_eggs.php
+++ cron/collect_eggs.php
@@ -10,4 +10,24 @@ $http = new Client('http://coop.apps.knpuniversity.com', array(
     )
 ));
 
+// run this code *before* requesting the eggs-collect endpoint
+$request = $http->post('/token', null, array(
+    'client_id'     => 'Cronjob App',
+    'client_secret' => 'a24e1199b705d5c6890c40c5fcf1fabb',
+    'grant_type'    => 'client_credentials',
+));
+
+// make a request to the token url
+$response = $request->send();
+$responseBody = $response->getBody(true);
+$responseArr = json_decode($responseBody, true);
+$accessToken = $responseArr['access_token'];
+
+$request = $http->post('/api/749/eggs-collect');
+$request->addHeader('Authorization', 'Bearer '.$accessToken);
+$response = $request->send();
+echo $response->getBody();
+
+echo "\n\n";
+
 
